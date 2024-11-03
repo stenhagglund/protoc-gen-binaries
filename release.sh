ce9f13216a86115663c91a971807d5c277d8cd82
Sten Hägglund <sten.hagglund@gmail.com>
2024-11-03 11:44:22 +0200

    chore: remove manual build scripts

--- release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd dist
-rm -f sha256.txt
-sha256sum $(ls . | grep -v sha256.txt) >> sha256.txt
-
-cd ..
-gh release create v28.2 ./dist/* --notes "Binaries built from https://github.com/protocolbuffers/protobuf/releases/tag/v28.2"
